facfabbeb01a776f251b2581bef54fe023e7e39d
CameronWiggins <cw1101046@student.francistuttle.edu>
2024-02-26 10:30:08 -0600

    half way done with comparing player and comp

--- src/script.js
+++ src/script.js
@@ -15,3 +15,44 @@ function getComputerChoice()
         return 'Scissors';
     }
 }
+
+function(playerSelection, computerSelection)
+{
+    playerSelection.toUppercase()
+    computerSelection.toUppercase()
+    
+    if(playerSelection === 'ROCK')
+    {
+        if (computerSelection === 'SCISSORS')
+        {
+            return 'You Win! Rock beats Scissors.'
+        }
+        else if(computerSelection === 'PAPER')
+        {
+            return 'You Lose! Paper beats Rock.'
+        }
+        else
+        {
+            return 'Its a tie, both of you chose Rock.'
+        }
+    }
+    else if(playerSelection === 'PAPER')
+    {
+        if (computerSelection === 'ROCK')
+        {
+            return 'You Win! Paper beats Rock.'
+        }
+        else if(computerSelection === 'SCISSORS')
+        {
+            return 'You Lose! Scissors beats Paper.'
+        }
+        else
+        {
+            return 'Its a tie, both of you chose Paper.'
+        }
+    }
+    else
+    {
+
+    }
+}
